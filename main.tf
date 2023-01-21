8c861609ee791e866c979c1e7345f700dda1f97f
Pshane92 <p.santhanakumar@outlook.com>
2023-01-21 20:37:33 +0530

    Added RTB & IGW to Main

--- main.tf
+++ main.tf
@@ -4,54 +4,79 @@ provider "aws" {
   //secret_key = ""
 }
 
-variable "vpc_subnet_cidr_block" {
-  description = "vpc subnet cidr block"
+variable "vpc_cidr_block" {}
+  //description = "vpc subnet cidr block"
   //default = "10.0.10.0/24" // will takes effect if there is no value defined. 
   //type = list(string) //restrict admin not pass like arrary etc.,
-  type = list(object({
-        cidr_block = string
-        name = string
-  }))
-}
+  //type = list(object({
+  //    cidr_block = string
+  //    name = string
+  //}))
+//}
+
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+
 
 //variable "environment" {
 //description = "development environment"
 //}
 
-resource "aws_vpc" "dev_vpc" {
-  cidr_block = var.vpc_subnet_cidr_block[0].cidr_block // "10.0.0.0/16"
+resource "aws_vpc" "myapp_vpc" { //"dev_vpc"
+  cidr_block = var.vpc_cidr_block //var.vpc_subnet_cidr_block[0].cidr_block // "10.0.0.0/16"
   tags = {
-    Name: var.vpc_subnet_cidr_block[0].name // "Development"
+    Name: "${var.env_prefix}-vpc" // ${}->to define a variable inside the "Quote" // var.vpc_subnet_cidr_block[0].name // "Development"
   }
 }
 
-resource "aws_subnet" "dev_subnet_1" {
-  vpc_id = aws_vpc.dev_vpc.id
-  cidr_block =  var.vpc_subnet_cidr_block[1].cidr_block // "10.0.10.0/24"
-  availability_zone = "ap-south-1a"
+resource "aws_subnet" "myapp_subnet_1" { //"dev_subnet_1"
+  vpc_id = aws_vpc.myapp_vpc.id
+  cidr_block = var.subnet_cidr_block // var.vpc_subnet_cidr_block[1].cidr_block // "10.0.10.0/24"
+  availability_zone = var.avail_zone // "ap-south-1a"
   tags = {
-    Name: var.vpc_subnet_cidr_block[1].name
+    Name: "${var.env_prefix}-subet-1" //var.vpc_subnet_cidr_block[1].name
   //vpc_env: "Dev"
   }
 }
 
-data "aws_vpc" "exsisting_vpc" {
-  default = true
+//data "aws_vpc" "exsisting_vpc" {
+//  default = true
+//}
+
+//resource "aws_subnet" "dev_subnet_2" {
+//vpc_id = data.aws_vpc.exsisting_vpc.id
+//cidr_block = "172.31.48.0/20"
+//availability_zone = "ap-south-1b"
+//tags = {
+//Name: "Development-Subnet-2"
+//}
+//}
+
+//output "dev_vpc_id" {
+//value  = aws_vpc.dev_vpc.id
+//}
+
+//output "dev_subnet_id" {
+//value  = aws_subnet.dev_subnet_1.id
+//}
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp_vpc.id
+
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+      Name: "${var.env_prefix}-rtb"
+    }
 }
 
-resource "aws_subnet" "dev_subnet_2" {
-  vpc_id = data.aws_vpc.exsisting_vpc.id
-  cidr_block = "172.31.48.0/20"
-  availability_zone = "ap-south-1b"
-  tags = {
-    Name: "Development-Subnet-2"
-  }
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp_vpc.id
+      tags = {
+      Name: "${var.env_prefix}-igw"
+    }
 }
 
-output "dev_vpc_id" {
-  value  = aws_vpc.dev_vpc.id
-}
-
-output "dev_subnet_id" {
-  value  = aws_subnet.dev_subnet_1.id
-}
--- terraform_dev.tfvars
+++ terraform_dev.tfvars
@@ -2,7 +2,13 @@
 //vpc_cidr_block = "10.0.0.0/16"
 //environment = "Development"
 //vpc_subnet_cidr_block = ["10.0.0.0/16","10.0.40.0/24"]
-vpc_subnet_cidr_block = [
-    {cidr_block = "10.0.0.0/16", name = "dev_vpc"},
-    {cidr_block = "10.0.40.0/24", name = "dev_subnet"}
-]
+//vpc_subnet_cidr_block = [
+//{cidr_block = "10.0.0.0/16", name = "dev_vpc"},
+//{cidr_block = "10.0.40.0/24", name = "dev_subnet"}
+//]
+
+
+vpc_cidr_block = "10.0.0.0/16"
+subnet_cidr_block = "10.0.40.0/24"
+avail_zone = "ap-south-1a"
+env_prefix = "dev"
